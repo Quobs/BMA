d8f378758a8ed0042a00424a79b403f96bb5bf0a
Jens-Christian Fischer <jens-christian.fischer@switch.ch>
2018-01-20 14:39:14 +0100

    Arbeit an math

--- make-it.sh
+++ make-it.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-asciidoctor-pdf -a toc -a pdf-stylesdir=source/theme -a pdf-style=pdf -a chapter-label=  -o arbeit.pdf source/index.adoc
+# asciidoctor-pdf -r asciidoctor-mathematical source/einleitung.adoc -o foo.pdf
+asciidoctor-pdf -r asciidoctor-mathematical  -a toc -a pdf-stylesdir=source/theme -a pdf-style=pdf -a chapter-label=  -o arbeit.pdf source/index.adoc
 open arbeit.pdf
